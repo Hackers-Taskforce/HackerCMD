92660f0d5ad96bd3aa0c65b49e3b8fd6039114d4
ChickenWithACrown <125937747+ChickenWithACrown@users.noreply.github.com>
2025-01-14 15:48:01 -0800

    Create thank_you.bat

---
+++ thank_you.bat
@@ -0,0 +1,22 @@
+@echo off
+cls
+color 0A
+
+:: Flashy thank you message
+:start
+echo.
+echo ********************** THANK YOU ***********************
+echo.
+echo To everyone who contributed to the making of HackerCMD!
+echo.
+echo Special thanks to:
+echo - ChickenWithACrown
+echo - FliksTrickshots
+echo.
+echo Your hard work and dedication made this possible!
+echo.
+echo ***********************************************************
+echo.
+ping localhost -n 1 > nul
+cls
+goto start
